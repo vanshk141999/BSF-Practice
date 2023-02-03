2a324d5c0606ccca3bcc176eb0a0b9fbb9507dee
Vansh <kvansh297@gmail.com>
2023-02-03 17:02:26 +0530

    statics & constants

--- php_oops/messages.php
+++ php_oops/messages.php
@@ -24,17 +24,7 @@
         }
     }
 
-    class Staff{
-        protected $members = [];
 
-        public function __construct($members = []){
-            $this->members = $members;
-        }
-
-        public function add(Person $person){
-            $this->members[] = $person;
-        }
-    }
 
 $vansh = new Person("Vansh");
 
---
+++ php_oops/statics & constants.php
@@ -0,0 +1,19 @@
+<?php
+
+//By default functions are dynamic
+
+    class Math{
+        public static function add(...$nums){
+            return array_sum($nums);
+        }
+    }
+
+    // $math = new Math();
+
+    // var_dump($math->add(4,3,2,4,1,2,3,4));
+
+    echo Math::add(4,3,2,4,1,2,3,4); //we are accessing add method from anywhere and this will become a global function
+
+    
+
+?>
