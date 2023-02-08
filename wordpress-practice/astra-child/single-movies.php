7f9f044ce420551e23fd327aced51dfc3ff7ca86
Vansh <kvansh297@gmail.com>
2023-02-08 17:21:00 +0530

    Template Heirarchy

---
+++ wordpress-practice/astra-child/single-movies.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * CPT movies single template.
+ */
+
+        get_header();
+
+            the_title();
+            the_content();    
+
+        get_footer();
+
+?>
---
+++ wordpress-practice/astra-child/taxonomy-course-test2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Courses taxonomy template for term test2.
+ */
+
+    get_header();
+
+        echo "<h1>Courses taxonomy archive for test2</h1>";
+
+    get_footer();
+
+?>
