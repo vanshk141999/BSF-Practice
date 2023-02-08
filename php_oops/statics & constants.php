2a3e08a1fa8cef8e514ee814ceb6b34d4abf74e6
Vansh <kvansh297@gmail.com>
2023-02-08 17:07:54 +0530

    Corrected the Comments in statics & constants.php

--- php_oops/statics & constants.php
+++ php_oops/statics & constants.php
@@ -1,6 +1,8 @@
 <?php
 
-//By default functions are dynamic
+/*
+* By default functions are dynamic
+*/
 
     class Math{
         public static function add(...$nums){
@@ -8,11 +10,11 @@
         }
     }
 
-    // $math = new Math();
+    $math = new Math();
 
-    // var_dump($math->add(4,3,2,4,1,2,3,4));
+    var_dump($math->add(4,3,2,4,1,2,3,4));
 
-    echo Math::add(4,3,2,4,1,2,3,4); //we are accessing add method from anywhere and this will become a global function
+    echo Math::add(4,3,2,4,1,2,3,4); // we are accessing add method from anywhere and this will become a global function.
 
     
 
