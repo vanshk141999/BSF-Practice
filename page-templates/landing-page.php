97f9ecd5f166f25501353b1f7d2716f779004316
Vansh <kvansh297@gmail.com>
2023-02-09 12:59:27 +0530

    Added template files

---
+++ page-templates/landing-page.php
@@ -0,0 +1,8 @@
+	
+<?php /* Template Name: landing-page */ ?>
+
+<?php
+
+    echo "<center><h1>SALE</h1></center>"
+
+?>
---
+++ page-templates/page-about.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: about */ ?>
+
+<?php //get_header(); ?>
+  
+<?php 
+    the_title();
+    // the_content();   
+?>
+  
+<?php //get_sidebar(); ?>
+<?php //get_footer(); ?>
