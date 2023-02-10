2f71489a08b27407c7ae22a418901189abb37daf
Vansh <kvansh297@gmail.com>
2023-02-10 10:41:18 +0530

    Initial Commit

---
+++ new-plugin/new-plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: New Plugin
+ * Description: My first Plugin!
+ * Version:     1.0
+ * Author:      Vansh Kapoor
+ * Author URI:  https://vanshk141999.github.io/
+ * License:     GPLv2 or later
+ */
+
+ echo "<div class='notice'>This is a notice for my plugin</div>";
+
+ ?>
